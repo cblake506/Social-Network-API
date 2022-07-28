777fafb9f5744f4e3de37da9e8d94fc0059b605f
Connor Blake <cblake506@gmail.com>
2022-07-27 23:19:29 -0500

    added routes

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+const {
+  getUsers,
+  getSingleUser,
+  createUser,
+  deleteUser,
+} = require('../../controllers/userController');
+
+router.route('/').get(getUsers).post(createUser);
+
+router.route('/:userId').get(getSingleUser).delete(deleteUser);
+
+router.route('/:userId/assignments').post(addAssignment);
+
+router.route('/:userId/assignments/:assignmentId').delete(removeAssignment);
+
+module.exports = router;
