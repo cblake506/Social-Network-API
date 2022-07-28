ffe32a94b878c92b335ac1e1d3f9d6ee6a1b9f62
Connor Blake <cblake506@gmail.com>
2022-07-27 23:33:07 -0500

    fixed file names

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -7,10 +7,8 @@ const {
   deleteThought,
 } = require('../../controllers/thoughtController.js');
 
-// /api/thoughts
 router.route('/').get(getThoughts).post(createThought);
 
-// /api/thoughts/:thoughtId
 router
   .route('/:thoughtId')
   .get(getSingleThought)
